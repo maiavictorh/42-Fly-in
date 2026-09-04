140f566bcd222bdfd809fbd5560b92b549ace49e
Torugo <maiavictorh@gmail.com>
2026-09-04 16:36:42 +0100

    starting render

--- src/renderer.py
+++ src/renderer.py
@@ -10,3 +10,19 @@ class Renderer:
 
     def run(self) -> None:
         pygame.init()
+        screen = pygame.display.set_mode((1280, 720))
+        clock = pygame.time.Clock()
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            screen.fill((30, 30, 30))
+            self._draw_graph(screen)
+
+            pygame.display.flip()
+            clock.tick(30)
+
+        pygame.quit()
